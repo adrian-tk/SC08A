d33b17e271e74ed066e388cfd1a2944579ac7d7f
domel <adrian.tk@gmail.com>
2022-07-05 23:24:40 +0200

    first test ready

--- tests/SC08A_test.py
+++ tests/SC08A_test.py
@@ -13,7 +13,11 @@ class TestConvToBin(unittest.TestCase):
         datas.insert(0,{"channel": 0, "speed": 0.0, "scale": 0.0, "exp_val": b'\xe0\x00\x00\x00'})
         datas.insert(1,{"channel": 1, "speed": 0.0, "scale": 0.0, "exp_val": b'\xe1\x00\x00\x00'})
         datas.insert(2,{"channel": 8, "speed": 0.0, "scale": 0.0, "exp_val": b'\xe8\x00\x00\x00'})
-        datas.insert(3,{"channel": 2, "speed": 2001.1, "scale": 0.0, "exp_val": b'\xe2\x00\x00\x00'})
+        datas.insert(3,{"channel": 2, "speed": 1.0, "scale": 0.0, "exp_val": b'\xe2\x00\x00\x00'})
+        datas.insert(4,{"channel": 3, "speed": 1.0, "scale": 1.0, "exp_val": b'\xe3\x00\x01\x00'})
+        datas.insert(5,{"channel": 4, "speed": 2.0, "scale": 0.5, "exp_val": b'\xe4\x00\x01\x00'})
+        datas.insert(6,{"channel": 5, "speed": 8000.0, "scale": 1.0, "exp_val": b'\xe5}\x00\x00'})
+        datas.insert(7,{"channel": 5, "speed": 8001.0, "scale": 1.0, "exp_val": b'\xe5}\x00\x00'})
         for i in datas:
             with self.subTest(i=i):
                 self.assertEqual(SC08A.Servo.pack_data(self,
@@ -21,7 +25,6 @@ class TestConvToBin(unittest.TestCase):
                     i["speed"], #input
                     i["scale"],),  #input
                     i["exp_val"]) #output
-                print (i["speed"]) 
 
 if __name__=='__main__':
     unittest.main()
