03163462030eb36dbfb4d6fc147b39ed7b4f8135
adrian.tk <adrian.tk@gmail.com>
2022-08-11 15:47:03 +0100

    auto path

--- website/mysite/wsgi.py
+++ website/mysite/wsgi.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 import sys
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/home/pi/repo/SC08A/website')
+#sys.path.append('/home/pi/repo/SC08A/website')
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
